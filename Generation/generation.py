babed9603322afbdd5c9657d9d49a58c4cb236a6
Ankuf <rishu.samanta@gmail.com>
2024-02-17 17:28:59 -0500

    generation endpoint update

--- Generation/generation.py
+++ Generation/generation.py
@@ -3,7 +3,7 @@ from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 
 class LLM():
-    def __init__(self, api_key="ibe3vQcJcEoA9CyAHc7amv6iijPYzoQT", model="mistral-medium"):
+    def __init__(self, api_key="gwGmq0uYH2PWtj3ZnuNpDXeCgtqaXnOf", model="mistral-medium"):
         self.model = model
         self.api_key = api_key
         self.client = MistralClient(api_key=self.api_key)
@@ -12,8 +12,10 @@ class LLM():
         return self.extract_content(self.client.chat(model=self.model, messages=self.construct_message_RAG(prompt)))    
     
     def chat(self, prompt):
-        print(prompt)
         return self.extract_content(self.client.chat(model=self.model, messages=self.construct_message_reg(prompt)))    
+    
+    def intent(self, prompt):
+        return self.extract_content(self.client.chat(model=self.model, messages=self.construct_message_intent(prompt)))
         
     def stream(self, prompt):
         return [self.extract_content(completion) for completion in self.client.chat_stream(model=self.model, messages=self.construct_message_RAG(prompt))]
@@ -33,5 +35,14 @@ class LLM():
         ]
         return messages
     
+    def construct_message_intent(self, prompt):
+        messages = [
+            ChatMessage(role="system", content="You are an intent classifier bot as part of Retrieval Augmented Generation System. Your task is to assess user query intent and categorize the query into one of the following predefined categories: 'generic question', specific question requiring information', and 'generic conversation'"),
+            ChatMessage(role="system", content="An example of a 'generic question' is 'what is the weather?' This does not require is to reference any knowledge from a database, but rather a general question that a RAG system likely would not have an answer for'. An example of a 'specific question requiring information' is 'How does self-attention work?' In this case, it is asking a question that requires factual information and generally is better to cite sources for. 'Generic conversation' is any other query that you assess would not need retrieved information to answer"),
+            ChatMessage(role="system", content="Make sure to respond ONLY with one of the 3 classifications: 'generic question', 'specific question requiring information', and 'generic conversation'"),
+            ChatMessage(role="user", content=prompt)
+        ]
+        return messages
+
     def extract_content(self, chat_response):
         return chat_response.choices[0].message.content
